0b11306909a31148e0b7a0cecb476d369a898021
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-01-28 22:03:41 -0600

    sample image

---
+++ Frontend/sample-img.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import { Image } from 'react-native'
+
+const ImagesExample = () => (
+   <Image source = {require('./img/sample.png')} />
+)
+export default ImagesExample
