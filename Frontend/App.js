3c75023c74116316babba1fbffc39eb1b118c73b
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-01-28 22:16:10 -0600

    restructure home page

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,40 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text, Image } from 'react-native';
 import { Provider as PaperProvider, Button, List } from 'react-native-paper';
-import ImagesExample from './sample-img.js';
 import BottomNav from './BottomNav';
 
 export default function App() {
   
   return (
     <PaperProvider>
-      <View style={styles.container}>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <View style={{flex:1, alignItems:'center'}}>
-            <Button disabled={true}>
-            </Button>
-          </View>
-          <View style={{flex:1, alignItems:'center'}}>
-            <Button icon="settings-helper" style={{backgroundColor: 'gray', textColor: 'white'}} onPress={() => console.log('Pressed')}>
-              Settings
-            </Button>
-          </View>
-        </View>
-
-        <View style={{flex: 1, flexDirection: 'column', bottom: 325}}>
-          <View>
-            <Text style={{fontWeight: 'bold', fontSize: 30, right: 50, fontStyle: 'italic'}}>Good morning David!</Text>
-          </View>
-          <View>
-            <ImagesExample/>
-          </View>
-          <View>
-            <Text>You're X points away from the next rank!</Text>
-          </View>
-          <Text style={{fontWeight: 'bold', top: 30, fontSize: 20}}>Top Events in your Area</Text>
-        </View>
-        <StatusBar style="auto" />
-      </View>
       <BottomNav></BottomNav>
     </PaperProvider>
   );
