97541835511e6aa66aa5b4e9b741d624811b3323
Joshua Huang <Paralysing@users.noreply.github.com>
2023-01-29 07:02:30 -0600

    Test

--- Frontend/ProfileComponent.js
+++ Frontend/ProfileComponent.js
@@ -2,56 +2,93 @@ import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Provider as PaperProvider, Button, List, Avatar, DataTable } from 'react-native-paper';
+import 'localstorage-polyfill';
 
-const ProfileComponent = () => {
+
+
+class ProfileComponent extends React.Component {
   //read in stuff
-    const [expanded, setExpanded] = React.useState(true);
-    const handlePress = () => setExpanded(!expanded);
 
-    const arr1 = [1,2,3];
-    const arr2 = [4,5,6];
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+      activityLength: 0,
+      points: 0,
+    }
+  }
+
+  componentDidMount() {
+    let events = [];
+
+    fetch('https://mh-api.owl.moe/api/v1/user/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: localStorage.username,
+        password: localStorage.password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    }).then(response => response.json()).then(data => {
+      fetch('https://mh-api.owl.moe/api/v1/user/get_data', {
+        method: 'GET',
+        credentials: 'include'
+      }).then(response => response.json()).then(data => {
+        this.setState({ events: data.event_data.past_event_data });
+        this.setState({ activityLength: data.event_data.past_event_data.length });
+        this.setState({ points: data.points });
+      });
+    });
+  }
+
+
+  render() {
+    let arr2 = ['No rewards available'];
     return (
-        <PaperProvider>
-          <View style={styles.container}>
-            <View>
-                <Avatar.Text size={150} backgroundColor='lavender' label="XD"/>
-            </View>
-            <View style={{flex: 1, flexDirection: 'row', top: 40}}>
-              <View style={{flex:1, alignItems:'center'}}>
-                <Text style={{fontWeight: 'bold', fontSize: 30}}>500</Text>
-                <Text style={{fontSize: 20}}>Completed Activities</Text>
-              </View>         
-              <View style={{flex:1, alignItems:'center'}}>
-                <Text style={{fontWeight: 'bold', fontSize: 30}}>500</Text>
-                <Text style={{fontSize: 20}}>Total Points</Text>
-              </View>   
-            </View>
-            <View style={{flex: 1, flexDirection: 'row', bottom: 100}}>
-                <View style={{flex:1, alignItems:'center'}}>
-                    <Text style={{fontSize: 20}}>Past Activities</Text>
-                    <List.Accordion style={{width: 185}}>
-                        { 
-                            arr1.map((item) => (
-                                <List.Item description={item} />
-                            ))
-                        }
-                    </List.Accordion>
-                </View>
-                <View style={{flex:1, alignItems:'center'}}>
-                    <Text style={{fontSize: 20}}>Past Rewards</Text>
-                    <List.Accordion style={{width: 185}}>
-                        { 
-                            arr2.map((item) => (
-                                <List.Item description={item} />
-                            ))
-                        }
-                    </List.Accordion> 
-                </View>
-            </View>
-            <StatusBar style="auto" />
+      <PaperProvider>
+        <View style={styles.container}>
+          <View>
+              <Avatar.Text size={150} backgroundColor='lavender' label="XD"/>
           </View>
-        </PaperProvider>
-      );
+          <View style={{flex: 1, flexDirection: 'row', top: 40}}>
+            <View style={{flex:1, alignItems:'center'}}>
+              <Text style={{fontWeight: 'bold', fontSize: 30}}>{this.state.activityLength}</Text>
+              <Text style={{fontSize: 20}}>Completed Activities</Text>
+            </View>         
+            <View style={{flex:1, alignItems:'center'}}>
+              <Text style={{fontWeight: 'bold', fontSize: 30}}>{this.state.points}</Text>
+              <Text style={{fontSize: 20}}>Total Points</Text>
+            </View>   
+          </View>
+          <View style={{flex: 1, flexDirection: 'row', bottom: 100}}>
+              <View style={{flex:1, alignItems:'center'}}>
+                  <Text style={{fontSize: 20}}>Past Activities</Text>
+                  <List.Accordion style={{width: 185}}>
+                      { 
+                          this.state.events.map((item) => (
+                              <List.Item description={item.event_id} />
+                          ))
+                      }
+                  </List.Accordion>
+              </View>
+              <View style={{flex:1, alignItems:'center'}}>
+                  <Text style={{fontSize: 20}}>Past Rewards</Text>
+                  <List.Accordion style={{width: 185}}>
+                      { 
+                          arr2.map((item) => (
+                              <List.Item description={item} />
+                          ))
+                      }
+                  </List.Accordion> 
+              </View>
+          </View>
+          <StatusBar style="auto" />
+        </View>
+      </PaperProvider>
+    );
+  }
 }
 
 
