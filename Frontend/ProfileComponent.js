0ca76190cbac4248ccdfcd28a9ed09e9f034a3e9
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-01-29 12:57:09 -0600

    added format changes to profile

--- Frontend/ProfileComponent.js
+++ Frontend/ProfileComponent.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
+import  { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Provider as PaperProvider, Button, List, Avatar, DataTable } from 'react-native-paper';
 import 'localstorage-polyfill';
 
 
 
+
+
+
 class ProfileComponent extends React.Component {
   //read in stuff
+
 
   constructor(props) {
     super(props);
@@ -20,8 +25,10 @@ class ProfileComponent extends React.Component {
     }
   }
 
+
   componentDidMount() {
     let events = [];
+
 
     fetch('https://mh-api.owl.moe/api/v1/user/login', {
       method: 'POST',
@@ -48,8 +55,10 @@ class ProfileComponent extends React.Component {
   }
 
 
+
+
   render() {
-    let arr2 = ['No previously purchased awards'];
+    let arr2 = ['No rewards available','wee','ree','shee'];
     return (
       <PaperProvider>
         <View style={styles.container}>
@@ -60,23 +69,24 @@ class ProfileComponent extends React.Component {
             <Text></Text>
           </View>
           <View>
-              <Avatar.Text size={150} backgroundColor='lavender' label={this.state.initials}/>
+              <Avatar.Text size={150} backgroundColor='beige' label={this.state.initials}/>
           </View>
-          <View style={{flex: 1, flexDirection: 'row', top: 40}}>
+          <View style={{flex: 1, flexDirection: 'row', top: 40, backgroundColor: '#fff'}}>
             <View style={{flex:1, alignItems:'center'}}>
               <Text style={{fontWeight: 'bold', fontSize: 30}}>{this.state.activityLength}</Text>
               <Text style={{fontSize: 20, left: 20}}>Completed Activities</Text>
-            </View>         
+            </View>        
 
-            <View style={{flex:1, alignItems:'center'}}>
+
+            <View style={{flex:1, alignItems:'center',}}>
               <Text style={{fontWeight: 'bold', fontSize: 30}}>{this.state.points}</Text>
               <Text style={{fontSize: 20}}>Total Points</Text>
-            </View>   
+            </View>  
           </View>
-          <View style={{flex: 1, flexDirection: 'row', bottom: 100}}>
+          <View style={{flex: 1, flexDirection: 'row', bottom: 100,}}>
               <View style={{flex:1, alignItems:'center'}}>
                   <Text style={{fontSize: 20}}>Past Activities</Text>
-                  <View style={{width: 185, left: 25}}>
+                  <View style={{width: 190, left: 3, top: 7, backgroundColor: '#fff'}}>
                       {
                           this.state.events.map((item) => (
                               <List.Item description={item.name} key={item.event_id}/>
@@ -84,15 +94,19 @@ class ProfileComponent extends React.Component {
                       }
                   </View>
               </View>
-              <View style={{flex:1, alignItems:'center'}}>
-                  <Text style={{fontSize: 20, left: 15}}>Past Rewards</Text>
-                  <View style={{width: 185, left: 30}}>
-                      { 
-                          arr2.map((item) => (
-                              <List.Item description={item} key={item}/>
-                          ))
-                      }
-                  </View> 
+              <View style={{flex:1, alignItems:'center',backgroundColor: '#fff'}}>
+                  <Text style={{fontSize: 20, 
+                     backgroundColor: '#fff'}}>Past Rewards</Text>
+                  <View style={{width: 150, left: 8, top:36, backgroundColor: '#fff',}}>
+                        <Text>
+                          No Purchased Awards
+                        </Text>
+                  </View>
+                  <View style={{width: 185, left: 1, top: 38,  backgroundColor: '#fff'}}>
+                        <Text style={[styles.setColorWhite,styles.setFontSize]}>
+                        Beep
+                        </Text>
+                  </View>  
               </View>
           </View>
           <StatusBar style="auto" />
@@ -103,17 +117,28 @@ class ProfileComponent extends React.Component {
 }
 
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'lavender',
     alignItems: 'center',
     flexDirection: 'column',
     paddingTop: 50,
     borderWidth: 20,
-    borderColor: 'lavender',
-    borderTopWidth: 50
+    borderColor: 'lavender'
+  },
+  setColorWhite: {
+    color: '#fff'
+  },
+  setFontSize: {
+    fontSize: 82,
   }
+
+
 });
 
+
 export default ProfileComponent;
+
