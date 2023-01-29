cb21f1775c3dfe5fc130d5658111bcdd0785c197
Matthew Zhang <matthewzhang0226@yahoo.com>
2023-01-28 21:55:08 -0600

    main page updates

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { Provider as PaperProvider, Button, List } from 'react-native-paper';
+import ImagesExample from './sample-img.js'
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{flex:1, alignItems:'center'}}>
+            <Button disabled={true}>
+            </Button>
+          </View>
+          <View style={{flex:1, alignItems:'center'}}>
+            <Button icon="settings-helper" style={{backgroundColor: 'gray', textColor: 'white'}} onPress={() => console.log('Pressed')}>
+              Settings
+            </Button>
+          </View>
+        </View>
+
+        <View style={{flex: 1, flexDirection: 'column', bottom: 325}}>
+          <View>
+            <Text style={{fontWeight: 'bold', fontSize: 30, right: 50, fontStyle: 'italic'}}>Good morning David!</Text>
+          </View>
+          <View>
+            <ImagesExample/>
+          </View>
+          <View>
+            <Text>You're X points away from the next rank!</Text>
+          </View>
+          <Text style={{fontWeight: 'bold', top: 30, fontSize: 20}}>Top Events in your Area</Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
@@ -15,6 +43,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flexDirection: 'column',
+    paddingTop: 50
+  }
 });
